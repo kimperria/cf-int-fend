4467b77ae15345e66388e7ebdc27dca0b6068b9c
kimperria <kimwaweru100@gmail.com>
2025-07-20 10:32:52 +0300

    mod: upgrade UI and rendering logic
    change key from tag to inbuilt index parameter
    change chips paddding, border radius, text and font size and add transition

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -1,6 +1,6 @@
 import { Badge } from "./ui/badge";
 
-// Component props
+// Component props to implement type safety
 interface TagListProps {
   title: string;
   tags: string[];
@@ -12,10 +12,10 @@ export default function TagList({ title, tags }: TagListProps) {
     <div className="mt-8 px-6 max-w-5xl mx-auto">
       <div className="text-white text-lg font-semibold mb-4">{title}</div>
       <div className="flex flex-wrap gap-3">
-        {tags.map((tag) => (
+        {tags.map((tag, index) => (
           <Badge
-            key={tag}
-            className="bg-gray-800 text-white hover:bg-gray-700 cursor-pointer"
+            key={index}
+            className="bg-gray-800 text-white hover:bg-gray-700 cursor-pointer px-4 py-2 rounded-xl text-sm font-medium transition-colors duration-200"
           >
             {tag}
           </Badge>
